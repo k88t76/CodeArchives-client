9c490b3ec0566a81b9f3ae8d29ae5d3bd8562843
k88t76 <k88t76@outlook.jp>
2021-02-19 05:55:11 +0900

    test tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,11 @@
-const extendHeight = () => {
-  var height = {};
-  for (let i = 0; i < 100; i++) {
-    height[42 + i * 3] = String(42 + i * 3) + 'rem';
-  }
-  return height;
-};
-
 module.exports = {
   purge: ['./components/**/*.tsx', './pages/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      height: extendHeight(),
+      height: {
+        45: '45rem',
+      },
     },
   },
   variants: {
