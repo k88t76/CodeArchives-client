eb089686c2328a10f9d15b4d82f716e499b47a45
k88t76 <k88t76@outlook.jp>
2021-02-19 05:30:16 +0900

    fixed tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,3 @@
-const extendHeight = () => {
-  var height = {};
-  for (let i = 0; i < 100; i++) {
-    height[42 + i * 3] = String(42 + i * 3) + 'rem';
-  }
-  return height;
-};
-
 module.exports = {
   purge: ['./components/**/*.tsx', './pages/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
