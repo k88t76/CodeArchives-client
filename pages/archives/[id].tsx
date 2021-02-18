53b3d4e749a30f820926d9dce8e786ea5e1032df
k88t76 <k88t76@outlook.jp>
2021-02-19 06:22:36 +0900

    test cast

--- pages/archives/[id].tsx
+++ pages/archives/[id].tsx
@@ -33,7 +33,7 @@ export default function Content({
   });
 
   const [n, setN] = useState(45);
-  const [s, setS] = useState('h-45');
+  const [s, setS] = useState(`h-${String(45)}`);
 
   const [response, setResponse] = useState({
     type: '',
