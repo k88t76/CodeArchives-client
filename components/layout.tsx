898c4a8e36fad0b50b340846ca68084bfa033afa
k88t76 <k88t76@outlook.jp>
2021-02-24 00:43:07 +0900

    remove unnecessary code

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,30 +1,14 @@
 import Head from 'next/head';
-import Link from 'next/link';
-import Router from 'next/router';
-import Prism from '../public/js/prism.js';
 
 export const siteTitle = 'CodeArchives';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const handleBacktoHome = (e) => {
-    e.preventDefault();
-    setTimeout(Prism.highlightAll, 1);
-    Router.push('/');
-  };
-
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="Code Archives is a web service for storing your codes" />
-        <meta
-          property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
+        <title>Code Archives</title>
+        <meta name="description" content="Code Archives is a web service to store your codes" />
       </Head>
       <main>{children}</main>
     </>
