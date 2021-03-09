41ba5d24e5d0d9a7b212ff7820692a1b94ceb2fc
k88t76 <k88t76@outlook.jp>
2021-03-09 12:06:54 +0900

    fixed margin

--- components/form.tsx
+++ components/form.tsx
@@ -32,7 +32,7 @@ export const Form: React.FC<Props> = ({ path, handleSubmit, handleChange }) => {
         {path === 'signup' ? 'Sign up' : 'Please Sign in'}
       </div>
       <form action={`${url}/${path}`} method="post" onSubmit={handleSubmit} className="my-4">
-        <label className="block text-sm font-bold mb-2">Username</label>
+        <label className="block text-sm font-bold mb-1">Username</label>
         <input
           type="text"
           placeholder="Username"
@@ -42,7 +42,7 @@ export const Form: React.FC<Props> = ({ path, handleSubmit, handleChange }) => {
           className="shadow appearance-none border rounded w-full py-2 px-3"
         />
         <div className="mb-6">
-          <label className="block text-sm font-bold mb-2">Password</label>
+          <label className="block text-sm font-bold mt-2 mb-1">Password</label>
           <input
             type="password"
             name="password"
