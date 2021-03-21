bdd576b496eb43a0019212a6756a682323c0711a
k88t76 <k88t76@outlook.jp>
2021-03-22 04:12:17 +0900

    refactor field.tsx

--- components/molecules/languageSelect.tsx
+++ components/molecules/languageSelect.tsx
@@ -1,12 +1,40 @@
 import React, { memo, useEffect } from 'react';
+import { Archive } from '../../lib/archive';
 
 interface Props {
   language: string;
+  archive: Archive;
+  setArchive: any;
 }
 
-export const LanguageSelect: React.VFC<Props> = memo(({ language }) => {
+export const LanguageSelect: React.VFC<Props> = memo(({ language, archive, setArchive }) => {
+  const handleLanguage = () => {
+    const lan = (document.getElementById('language') as HTMLSelectElement).value;
+    setArchive({ ...archive, language: lan });
+    const target = document.getElementById('code') as HTMLDivElement;
+    target.className = 'language-' + lan;
+    if (lan == 'text') {
+      target.className = '';
+    }
+  };
+
+  const handleSetSelected = () => {
+    if (language) {
+      const option = document.getElementById(`${language}`) as HTMLOptionElement;
+      option.selected = true;
+    }
+  };
+
+  useEffect(() => {
+    handleSetSelected();
+  }, []);
+
   return (
-    <>
+    <select
+      id="language"
+      className="flex z-50 mt-0.5 mr-6 border-2 sm:w-64 w-20 px-2 h-10 rounded"
+      onChange={handleLanguage}
+    >
       {language === '' && <option>Language</option>}
       <option id="go" value="go">
         Go
@@ -59,6 +87,6 @@ export const LanguageSelect: React.VFC<Props> = memo(({ language }) => {
       <option id="text" value="text">
         TEXT
       </option>
-    </>
+    </select>
   );
 });
